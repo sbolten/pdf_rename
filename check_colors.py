3dce281a7d6df6d54ab3df7f78150918ee410f45
Your Name <you@example.com>
2025-11-21 12:57:28 +0100

    chore: remove temporary Flet attribute and color checking scripts.

--- check_colors.py
+++
@@ -1,5 +0,0 @@
-import flet as ft
-try:
-    print([c for c in dir(ft.Colors) if "SURFACE" in c])
-except Exception as e:
-    print(e)
--- check_usercontrol.py
+++
@@ -1,5 +0,0 @@
-import flet as ft
-try:
-    print(f"ft.UserControl: {ft.UserControl}")
-except AttributeError:
-    print("ft.UserControl does not exist")
